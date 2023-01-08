c8890a9460ca3ede0daf127b79cea0b360ebdb66
aapo Peltomäki <aapo.peltomaki@tuni.fi>
2023-01-09 01:10:03 +0200

    jotain

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,2 @@
 print("Hello world!")
-print("Hello darkness my old friend")
-<<<<<<< HEAD
-=======
-print("Hello darkness my old friend")
->>>>>>> 380b3e6c7b69204e182e6ab09da42426fb333590
+print("I've come to talk with you again")
